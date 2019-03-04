d21d27db8f6e16932f6ae6ce6a5ccde2aef5e263
Joel <joel.pedersen97@gmail.com>
2019-03-04 22:44:15 +0100

    added REST class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import  Login from './components/Login';
+import Login from './components/Login';
 import Startpage from './components/Startpage';
 import './App.scss';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/Header';
 import MyBookings from './components/MyBookings';
 import CurrentShowsPage from './components/CurrentShowsPage';
 import MovieInfo from './components/MovieInfo';
+import REST from './REST'
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <Header />
-        <Route exact path='/' component={Startpage} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/showtime' component={CurrentShowsPage} />
-        <Route exact path= '/film/id' component= {MovieInfo} />
-      </div>
+        <div className="App">
+          <Header />
+          <Route exact path='/' component={Startpage} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/showtime' component={CurrentShowsPage} />
+          <Route exact path='/film/id' component={MovieInfo} />
+        </div>
       </Router>
     );
   }
