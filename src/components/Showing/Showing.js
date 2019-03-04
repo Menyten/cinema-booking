3b2c2736f3413c6cc0231f650c263e237b0a32cd
Pontus Nilsson <p0nta_@hotmail.com>
2019-03-04 21:09:20 +0100

    beginning of showing

--- src/components/Showing/Showing.js
+++ src/components/Showing/Showing.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 export default class Showing extends Component {
-  render(){
-    return(
-      
+  render() {
+    return (
+      <Container>
+        <Row className="mt-5">
+          <h2 className="col-sm-12 cinemaDiv">
+            {this.showing === undefined ? '' : this.showing.film}
+          </h2>
+          <p className="col-sm-12 cinemaDiv text-center">
+            Datum: {this.showing === undefined ? '' : this.showing.date}
+          </p>
+          <p className="col-sm-12 cinemaDiv text-center">
+            Tid: {this.showing === undefined ? '' : this.showing.time}
+          </p>
+        </Row>
+        <Row className="pt-3 mt-4 text-center cinemaDiv">
+          <h2 className="col-12 pickTicketSize">Välj antal biljetter</h2>
+          <Col sm="12" md="4" className="mb-1">
+            <h4 className="personTicketSize">Vuxna (85kr)</h4>
+            <Button type="button" className="btn btn remove-adult remove-one selectButton selectButton1">-</Button>
+            <span className="adult">{this.countAdult}</span>
+            <Button type="button" className="btn btn add-adult add-one selectButton selectButton2">+</Button>
+          </Col>
+          <Col sm="12" md="4" className="mb-1">
+            <h4 className="personTicketSize">Barn (65kr)</h4>
+            <Button type="button" className="btn btn remove-kid remove-one selectButton selectButton1">-</Button>
+            <span className="kid">{this.countKid}</span>
+            <Button type="button" classname="btn btn add-kid add-one selectButton selectButton2">+</Button>
+          </Col>
+
+          
+        </Row>
+      </Container>
     )
   }
 
@@ -11,33 +41,9 @@ export default class Showing extends Component {
 
 /*
 
-<div>
-  ${this.modal}
-  <div class="row mt-5">
-    <h2 class="col-sm-12 cinemaDiv">
-      ${this.showing === undefined ? '' : this.showing.film }
-    </h2>
-    <p class="col-sm-12 cinemaDiv text-center">
-      Datum: ${this.showing === undefined ? '' : this.showing.date }
-    </p>
-    <p class="col-sm-12 cinemaDiv text-center">
-      Tid: ${this.showing === undefined ? '' : this.showing.time }
-    </p>
-  </div>
-  <div class="row pt-3 mt-4 text-center cinemaDiv">
-    <h2 class="col-12 pickTicketSize">Välj antal biljetter</h2>
-    <div class="col-sm-12 mb-1 col-md-4">
-      <h4 class="personTicketSize">Vuxna (85kr)</h4>
-      <button type="button" class="btn btn remove-adult remove-one selectButton selectButton1">-</button>
-      <span class="adult">${this.countAdult}</span>
-      <button type="button" class="btn btn add-adult add-one selectButton selectButton2">+</button>
-    </div>
-    <div class="col-sm-12 mb-1 col-md-4">
-      <h4 class="personTicketSize">Barn (65kr)</h4>
-      <button type="button" class="btn btn remove-kid remove-one selectButton selectButton1">-</button>
-      <span class="kid">${this.countKid}</span>
-      <button type="button" class="btn btn add-kid add-one selectButton selectButton2">+</button>
-    </div>
+      
+
+
     <div class="col-sm-12 mb-1 col-md-4">
       <h4 class="personTicketSize">Pensionär (75kr)</h4>
       <button type="button" class="btn btn remove-retired remove-one selectButton selectButton1">-</button>
