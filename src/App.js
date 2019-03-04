913d91e37081b3df04655bcdfd28050194d3d591
Joel <joel.pedersen97@gmail.com>
2019-03-04 11:04:55 +0100

    removed mainpage component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import Mainpage from './components/Mainpage';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Mainpage />
       </div>
     );
   }
