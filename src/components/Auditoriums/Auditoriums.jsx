f25c0431b4461a0aa9bfae1dee19049790ba15d7
Fanny <fanny_andersson@hotmail.com>
2019-03-04 21:29:24 +0100

    Add auditorium and seat components

---
+++ src/components/Auditoriums/Auditoriums.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Seat from '../Seat.index';
+
+const Auditoriums = () => {
+  return (
+    <div>
+      <h1>Salong</h1>
+      <Seat />
+      </div>
+  );
+};
+
+export default Auditoriums;
---
+++ src/components/Seat/Seat.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HistoryBookings.scss';
+
+const Seat = () => {
+  return (
+    <div>
+      Heres gonna be some seats
+    </div>
+  );
+};
+
+export default Seat;
