30085ec628efa9c62dc154bee02c56487658a338
Sudha Parimala <mailme.sudha.k@gmail.com>
2019-03-04 12:04:09 +0100

    Added Router to startpage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import Startpage from './components/Startpage';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
       <Router>
+      <div className="App">
         <Header />
         <Route exact path='/' component={Startpage} />
-      </Router>
       </div>
+      </Router>
     );
   }
 }
