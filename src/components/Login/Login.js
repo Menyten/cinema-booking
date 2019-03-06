6f98be3e4ade7ab29202b940d4e9e595e158f26d
Pontus Nilsson <p0nta_@hotmail.com>
2019-03-06 11:27:29 +0100

    input design

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,33 +1,43 @@
 import React from 'react';
-import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Container, Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import './loginstyle.scss'
 
 export default class Login extends React.Component {
   render() {
     return (
-      <Form className="Container">
-        <Row form>
-          <Col md={6}>
-            <FormGroup>
-              <Label className="emailColor">Email</Label>
-              <Input type="email"
-              className="form-control"
-              name="email"
-              id="exampleEmail"
-              placeholder="with a placeholder" />
-            </FormGroup>
-          </Col>
-          <Col md={6}>
-            <FormGroup>
-              <Label className="passwordColor">Password</Label>
-              <Input type="password" className="form-control password" name="password" id="examplePassword" placeholder="password placeholder" />
-            </FormGroup>
-          </Col>
-        </Row>
-             <Button className="submit-btn individualButton"> Logga in </Button>
-             <Button className="register-btn individualButton"> Registrera </Button>
-      </Form>
-  
+      <Container>
+        <Form className="Container">
+          <h3 className="frontParagraph mt-3">Logga in</h3>
+          <Row form>
+            <Col md={6}>
+              <FormGroup className="mt-3">
+                <Label className="emailColor">Email</Label>
+                <Input type="email"
+                  className="form-control"
+                  name="email"
+                  id="exampleEmail"
+                  placeholder="..." />
+              </FormGroup>
+            </Col>
+            <Col md={6} />
+          </Row>
+          <Row form>
+            <Col md={6}>
+              <FormGroup className="mt-3">
+                <Label className="passwordColor">Password</Label>
+                <Input type="password"
+                  className="form-control password"
+                  name="password" id="examplePassword"
+                  placeholder="..." />
+              </FormGroup>
+            </Col>
+            <Col md={6} />
+          </Row>
+          <Button className="submit-btn individualButton"> Logga in </Button>
+          <Button className="register-btn individualButton ml-1"> Registrera </Button>
+        </Form>
+      </Container>
+
     );
   }
 }
